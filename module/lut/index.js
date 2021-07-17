8cf6dec5eee44ddaa952803bfce0f9f3c53f2d4c
Hippo Spark (Arch) <hippospark@gmail.com>
2021-07-17 11:10:31 +0800

    采用新的目录结构后通过了单元测试。

--- module/lut/index.js
+++ module/lut/index.js
@@ -9,14 +9,36 @@ class LookupTable extends AbstractBaseLogicModule {
         // 模块参数
         let inputPinCount = this.getParameter('inputPinCount'); // 输入端口的数量
         let outputBitWidth = this.getParameter('outputBitWidth'); // 输出数据宽度
-        let
+
+        // dataTable 的结构为：
+        // [{address: Number, value: Number},...]
+        let dataTable = this.getParameter('dataTable');
+
+        // 将 dataTable 映射到一数组，数组大小为 2^inputPinCount
+        // | address | value |
+        // |---------|-------|
+        // |    0000 |     0 |
+        // |     ... |     . |
+        // |    1111 |     0 |
+
+        let rows = new Array(Math.pow(2, inputPinCount));
+
+        for (let dataRow of dataTable) {
+            let address = Number(dataRow.address);
+            let value = Number(dataRow.value);
+            let binary = Binary.fromInt32(value, outputBitWidth);
+            let signal = Signal.createWithoutHighZ(outputBitWidth, binary);
+            rows[address] = signal;
+        }
+
+        this.rows = rows;
 
         // 输出端口
-        this.addOutputPinByDetail('out', outputBitWidth);
+        this.pinOut = this.addPin('out', outputBitWidth, PinDirection.output);
 
         // 输入端口的名称分别为 in0, in1, ... inN
         let createInputPin = (idx) => {
-            this.addInputPinByDetail('in' + idx, outputBitWidth);
+            this.addPin('in' + idx, outputBitWidth, PinDirection.input);
         };
 
         // 输入端口
@@ -31,16 +53,16 @@ class LookupTable extends AbstractBaseLogicModule {
 
     // override
     updateModuleState() {
-//         let datas = this.inputPins.map(pin => {
-//             return pin.getData();
-//         });
-//
-//         let resultData = datas[0];
-//         for (let idx = 1; idx < datas.length; idx++) {
-//             resultData = Binary.and(resultData, datas[idx]);
-//         }
-//
-//         this.outputPins[0].setSignal(resultData);
+
+        let address = 0;
+        for (let idx = 0; idx < this.inputPins.length; idx++) {
+            let inputPin = this.inputPins[idx];
+            let bitValue = inputPin.getSignal().getBinary().toInt32();
+            address += Math.pow(2, idx) * bitValue;
+        }
+
+        let resultSignal = this.rows[address];
+        this.pinOut.setSignal(resultSignal);
     }
 }
 
